ef822490434b97924b639384fc26082c218828ab
lea <leandro.n.aguilera@gmail.com>
2022-03-07 20:03:17 -0300

    Middlware para comprobar el token

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -6,10 +6,11 @@ const { check } = require("express-validator");
 const { validarCampos } = require("../middlewares/validar-campos");
 
 const { getUsuarios, crearUsuario, actualizarUsuario, borrarUsuario } = require("../controllers/usuarios");
+const { validarJWT } = require("../middlewares/validar-jwt");
 
 const router = Router();
 
-router.get("/", getUsuarios);
+router.get("/", validarJWT, getUsuarios);
 
 router.post("/",
   [
@@ -23,14 +24,15 @@ router.post("/",
 
 router.put("/:id",
   [
+    validarJWT,
     check("nombre", "El nombre es obligatorio").not().isEmpty(),
-    check("role", "El role es obligatorio").isEmail(),
+    check("role", "El role es obligatorio").not().isEmpty(),
     check("email", "El email es obligatorio").isEmail(),
     validarCampos,
   ],
   actualizarUsuario
 );
 
-router.delete("/:id", borrarUsuario)
+router.delete("/:id", validarJWT, borrarUsuario)
 
 module.exports = router;
