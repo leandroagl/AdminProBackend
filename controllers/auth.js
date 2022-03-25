545099be50ffcddd2f8e2f5ba6b67d5eef32c44f
Leandro <leandro.n.aguilera@gmail.com>
2022-03-25 10:18:26 -0300

    Autenticacion con Google

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,6 +3,7 @@ const bcrypt = require("bcryptjs");
 
 const Usuario = require('../models/usuario');
 const { generarJWT } = require("../helpers/jwt");
+const { googleVerify } = require("../helpers/google-verify");
 
 
 
@@ -51,6 +52,55 @@ const login = async(req, res = response) => {
 
 }
 
+const googleSignIn = async( req, res = response ) => {
+
+    const googleToken = req.body.token;
+
+    try {
+
+        const { name, email, picture } = await googleVerify( googleToken );
+
+        const usuarioDB = await Usuario.findOne({ email });
+        let usuario;
+
+        if (!usuarioDB) {
+            // Si no existe el usuario
+            usuario = new Usuario({
+                nombre: name,
+                email,
+                password: '@@@',
+                img: picture,
+                google: true
+            })
+        } else {
+            // Existe usuario
+            usuario = usuarioDB;
+            usuario.google = true;
+        }
+
+        // Guardar en DB
+        await usuario.save();
+
+        // Generar Token
+        const token = await generarJWT(usuario.id);
+
+        res.json({
+            ok: true,
+            token
+        })
+
+    } catch (error) {
+        res.status(401).json({
+            ok: false,
+            msg: 'Token inválido'
+        })
+    }
+    
+
+}
+
+
 module.exports = {
-    login
+    login,
+    googleSignIn
 }
